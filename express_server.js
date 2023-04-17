0be1d471fef940c835e496e09b8f9ea79da47d55
SirGraybon <graydonritchie@gmail.com>
2023-04-17 19:00:10 -0400

    implemented get for urls_show.js

--- express_server.js
+++ express_server.js
@@ -18,10 +18,18 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase}
-  res.render("urls_index.ejs", templateVars)
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index.ejs", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  console.log(req.params.id);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// console.log(urlDatabase.b2xVn2)
