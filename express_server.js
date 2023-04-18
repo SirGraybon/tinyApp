81412c46ea9fe0d7dfb4ce2f409bf1fea72fc37d
SirGraybon <graydonritchie@gmail.com>
2023-04-17 22:05:03 -0400

    added string generator file

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const generateRandomString = require('./generator')
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -22,8 +24,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index.ejs", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  console.log(req.params.id);
+
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
@@ -32,4 +38,8 @@ app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// console.log(urlDatabase.b2xVn2)
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
