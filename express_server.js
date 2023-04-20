afdf7328b88986da937d65d067ac02f5dfd997e7
SirGraybon <graydonritchie@gmail.com>
2023-04-20 15:35:35 -0400

    exported helper functions to own file.

--- express_server.js
+++ express_server.js
@@ -22,45 +22,7 @@ app.use(cookieSession({
 // currentUser variable read form cookie
 //ngrok
 
-///////////////Functions///////////////////////
-// const generateRandomString = function() {
-//   let string = "";
-//   for (let i = 6; i > 0; i--) {
-//     let setter = Math.floor(Math.random() * 3);
-//     if (setter === 0) {
-//       let charCodeIndex = Math.floor(Math.random() * (58 - 48) + 48);
-//       string += String.fromCharCode(charCodeIndex);
-//     }
-//     if (setter === 1) {
-//       let charCodeIndex = Math.floor(Math.random() * (91 - 65) + 65);
-//       string += String.fromCharCode(charCodeIndex);
 
-//     }
-//     if (setter === 2) {
-//       let charCodeIndex = Math.floor(Math.random() * (123 - 97) + 97);
-//       string += String.fromCharCode(charCodeIndex);
-//     }
-//   }
-//   return string;
-// };
-
-// const findUser = function(id, obj) {
-//   for (const users in obj) {
-//     if (obj[users].email === id) {
-//       return obj[users];
-//     }
-//   }
-//   return false;
-// };
-
-// const getUserURLs = function(user) {
-//   const userURLs = {}
-//   for (const id in urlDatabase ){
-//     if (urlDatabase[id].userID === user) {
-//       userURLs[id] = urlDatabase[id]    }
-//   }
-//   return userURLs
-// }
 ///////////////objects///////////////////////
 
 const urlDatabase = {
