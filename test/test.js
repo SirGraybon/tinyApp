619c006261e6dc28fec0d669be1600fcbcdf3de3
SirGraybon <graydonritchie@gmail.com>
2023-04-20 15:55:42 -0400

    wrote out test cases

--- test/test.js
+++ test/test.js
@@ -1,29 +1,34 @@
-const assert = require('chai');
+const assert = require('chai').assert;
 const { generateRandomString, findUser, getUserURLs } = require('../functions');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
-
-describe("#findUser", function(){
-  it ("should return user object if valid"), function(){
-    const user = findUser("userRandomID", testUsers)
-    const expected =  {
-      id: "userRandomID", 
-      email: "user@example.com", 
+describe("#findUser", function() {
+  it("should return user object if valid", function() {
+    const user = findUser("user@example.com", testUsers);
+    const expected = {
+      id: "userRandomID",
+      email: "user@example.com",
       password: "purple-monkey-dinosaur"
-    }
+    };
+
+    assert.deepEqual(user, expected);
+  });
+  it ("should return false if usser not in DB", function(){
+    const user = findUser("user@example.ca", testUsers)
+    const expected =  false
 
     assert.deepEqual(user, expected)
-  }
-})
+  })
+});
